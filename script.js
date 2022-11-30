1814db46dfc8c8c776bcd7f4bad0d189e5ff7245
Phil J Gray <phl.j.gray@gmail.com>
2022-11-29 19:12:27 -0800

    Intital commit
    Design based off of minimal-portfolio from Frontend Mentor

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+//Variables
+//Nav
+const navList = document.querySelector(".nav__list");
+const navIcon = document.querySelector(".nav__icon");
+const closeIcon = document.querySelector(".close__icon");
+
+//Form
+const form = document.querySelector(".form");
+const input = document.querySelector(".contact__input");
+const formButton = document.querySelector(".contact__input--btn");
+const emailInput = document.querySelector(".contact__input--email");
+const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+// Function
+
+//Nav Toggle
+
+const toggleOn = function () {
+  if ((navList.style.diplay = "none")) {
+    navList.style.display = "flex";
+    navIcon.style.display = "none";
+    closeIcon.style.display = "block";
+  }
+};
+
+const toggleOff = function () {
+  if ((navList.style.diplay = "flex")) {
+    navList.style.display = "none";
+    navIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  }
+};
+
+// Input Error
+// const inputError = function (input, pattern) {
+//   // if (emailInput != emailPattern) {
+//   //   emailInput.border.border = "hsl(0 90% 57%)";
+//   // }
+//   console.log("submit");
+// };
+
+// Event Listeners
+
+navIcon.addEventListener("click", toggleOn);
+closeIcon.addEventListener("click", toggleOff);
+
+// formButton.addEventListener("click", inputError);
