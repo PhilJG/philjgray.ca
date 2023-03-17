fd59821d5992840bcc5e4e2ddf2526e222e710f7
Phil J Gray <phl.j.gray@gmail.com>
2023-03-16 22:06:22 -0700

    eleventy added

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
+  
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
+};
